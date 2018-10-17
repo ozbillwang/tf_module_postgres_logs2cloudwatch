f4a570db6b90e8de839732fbcd064c592092e8c5
Bill Wang <bill.wang@team.telstra.com>
2018-10-17 17:13:01 +1100

    Add feature to push log events

--- source/main.py
+++ source/main.py
@@ -5,7 +5,7 @@
 # (optional) LOG_GROUP
 
 from __future__ import print_function
-
+from botocore.client import ClientError
 import sys
 import os
 import time
@@ -14,6 +14,35 @@ import boto3
 
 rds = boto3.client('rds')
 logs = boto3.client('logs')
+s3 = boto3.resource('s3')
+
+sts = boto3.client('sts')
+account_id = sts.get_caller_identity()["Account"]
+region = boto3.session.Session().region_name
+
+
+def download_s3_file(bucket_name, file_name):
+    """
+    download s3 file, if bucket is not exist, create it first.
+    """
+
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+    except ClientError:
+        s3.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={'LocationConstraint': region})
+
+    try:
+        s3.Bucket(bucket_name).download_file(file_name, file_name)
+    except ClientError:
+        print("The log state file does not exist.")
+
+
+def upload_s3_file(bucket_name, file_name):
+    """
+    upload file to s3
+    """
 
 
 def manage_log_group(log_group, log_stream):
@@ -37,14 +66,20 @@ def manage_log_group(log_group, log_stream):
     sum = 0
     for stream in response['logStreams']:
         if log_stream == stream['logStreamName']:
-            sequence_token = stream['uploadSequenceToken']
+            if 'uploadSequenceToken' in stream:
+                sequence_token = stream['uploadSequenceToken']
             sum += 1
 
     if not sum:
         response = logs.create_log_stream(
             logGroupName=log_group, logStreamName=log_stream)
 
-    return sequence_token
+    try:
+        sequence_token
+        return sequence_token
+    except NameError:
+        return "None"
+
 
 def lambda_handler(event, context):
     """
@@ -76,8 +111,12 @@ def lambda_handler(event, context):
 
     sequence_token = manage_log_group(LOG_GROUP, LOG_STREAM)
 
+    bucket_name = "{}_rds_logs_state".format(account_id)
+    file_name = "{}_rds_log_state".format(DB_INSTANCE_IDENTIFIER)
+    download_s3_file(bucket_name, file_name)
+
     try:
-        f = open(DB_INSTANCE_IDENTIFIER + '_rds_log_state', 'r')
+        f = open(file_name, 'r')
         lastReadDate = int(f.readline())
         readState = f.readline()
         f.close()
@@ -119,22 +158,32 @@ def lambda_handler(event, context):
                 # LogFileData sends all entries as a single string.
                 # Split it up into a list to be able to append text to start
                 for entry in logLines:
-                    message = "[{}] {}".format(
-                        logFile['LogFileName'], entry)
-                    response = logs.put_log_events(
-                        logGroupName=LOG_GROUP,
-                        logStreamName=LOG_STREAM,
-                        logEvents=[{
-                            'timestamp': lastReadDate,
-                            'message': message
-                        }],
-                        sequenceToken=sequence_token)
-                    sequence_token = response['nextSequenceToken']
+                    message = "[{}] {}".format(logFile['LogFileName'], entry)
+                    if sequence_token == "None":
+                        event_response = logs.put_log_events(
+                            logGroupName=LOG_GROUP,
+                            logStreamName=LOG_STREAM,
+                            logEvents=[{
+                                'timestamp': lastReadDate,
+                                'message': message
+                            }])
+                    else:
+                        event_response = logs.put_log_events(
+                            logGroupName=LOG_GROUP,
+                            logStreamName=LOG_STREAM,
+                            logEvents=[{
+                                'timestamp': lastReadDate,
+                                'message': message
+                            }],
+                            sequenceToken=str(sequence_token))
+                    sequence_token = event_response['nextSequenceToken']
 
             readState[logFile['LogFileName']] = response['Marker']
 
-    f = open(DB_INSTANCE_IDENTIFIER + '_rds_log_state', 'w')
+    f = open(file_name, 'w')
     f.write(str(lastReadDate))
     f.write("\n")
     f.write(str(readState))
     f.close()
+
+    upload_s3_file(bucket_name, file_name)
